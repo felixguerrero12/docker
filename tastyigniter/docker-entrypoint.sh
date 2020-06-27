6a316c5fe30680d35777161f12aac3e5c91f2cf3
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2020-06-27 10:48:34 -0500

    Update docker-entrypoint.sh

--- tastyigniter/docker-entrypoint.sh
+++ tastyigniter/docker-entrypoint.sh
@@ -5,5 +5,6 @@ curl -o /var/www/html/tastyigniter.zip -fSL "https://codeload.github.com/tastyig
 unzip /var/www/html/tastyigniter.zip -d /var/www/html
 chown -R www-data:www-data /var/www/html/
 mv -v /var/www/html/setup-master/* /var/www/html/
+echo 'echo "* * * * * php artisan schedule:run >> /dev/null 2>&1"' > /etc/cron.d/artisan
 rm /var/www/html/tastyigniter.zip
 exec "$@"
